89f8b5fa3c7dccfb49210b89c001c05f5afc0e2b
ddogming <tpdus9170@naver.com>
2025-02-10 14:27:42 +0900

    dd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // base: 'https://ddogming.github.io/product/'
+  base: process.env.NODE_ENV === 'production' ? '/product/' : '/'
 })
