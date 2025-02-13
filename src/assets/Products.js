7288b18defb6ea2bd705d2917ca05ace35c2fe23
ddogming <tpdus9170@naver.com>
2025-02-13 11:38:46 +0900

    ssa

--- src/assets/Products.js
+++ src/assets/Products.js
@@ -4,14 +4,14 @@ export const products = [
     {id:3, name: '1955버거', price:3000, image:'./images/3.png',category:'Best'},
     {id:4, name: '슈슈버거', price:5000, image:'./images/4.png',category:'Set'},
     {id:5, name: '새우버거', price:4000, image:'./images/6.png',category:'Best'},
-    {id:5, name: '치즈버거', price:4000, image:'./images/7.png',category:'Set'},
-    {id:5, name: '치킨버거', price:8000, image:'./images/8.png',category:'Best'},
-    {id:5, name: '상하이버거', price:7500, image:'./images/9.png',category:'Set'},
-    {id:5, name: '토마토버거', price:8000, image:'./images/10.png',category:'Best'},
-    {id:5, name: '머쉬룸버거', price:5500, image:'./images/11.png',category:'New'},
-    {id:5, name: '베이컨버거', price:6000, image:'./images/12.png',category:'New'},
-    {id:5, name: '탄산음료', price:2000, image:'./images/13.png',category:'Beverage'},
-    {id:5, name: '감자튀김', price:2000, image:'./images/14.png',category:'Side'},
-    {id:5, name: '치즈스틱', price:2000, image:'./images/15.png',category:'Side'},
+    {id:6, name: '치즈버거', price:4000, image:'./images/7.png',category:'Set'},
+    {id:7, name: '치킨버거', price:8000, image:'./images/8.png',category:'Best'},
+    {id:8, name: '상하이버거', price:7500, image:'./images/9.png',category:'Set'},
+    {id:9, name: '토마토버거', price:8000, image:'./images/10.png',category:'Best'},
+    {id:10, name: '머쉬룸버거', price:5500, image:'./images/11.png',category:'New'},
+    {id:11, name: '베이컨버거', price:6000, image:'./images/12.png',category:'New'},
+    {id:12, name: '탄산음료', price:2000, image:'./images/13.png',category:'Beverage'},
+    {id:13, name: '감자튀김', price:2000, image:'./images/14.png',category:'Side'},
+    {id:14, name: '치즈스틱', price:2000, image:'./images/15.png',category:'Side'}
 ];
 export const categories = ['All','Best','New','Set','Beverage','Side'];
